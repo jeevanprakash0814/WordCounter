8d3819275577ef2887419ba4db03738ca513c345
jeevanprakash0814 <jeevanpraks@gmail.com>
2019-12-06 20:56:49 -0600

    Developed a simple word counting tool that can be run in terminal.

---
+++ src/WordCounter.java
@@ -0,0 +1,48 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class WordCounter {
+
+    public WordCounter()
+    {
+        System.out.println(totalWords());
+    }
+
+    private static int totalWords()
+    {
+        return wordsInLine(populateTextFile());
+    }
+
+    private static String populateTextFile()
+    {
+        FileWriter inputWriter;
+        Scanner inputScanner = new Scanner(System.in);
+        String inputStr = "";
+
+        try
+        {
+            inputStr = inputScanner.nextLine();
+            inputWriter = new FileWriter("/Users/jeevanprakash/Documents/IdeaProjects/WordCounter/inputText.txt");
+            inputWriter.write(inputStr);
+            inputScanner.close();
+            inputWriter.close();
+        }
+        catch (IOException i){
+            System.out.println("Error " + i.getMessage());
+        }
+        return inputStr;
+    }
+
+    private static int wordsInLine(String str)
+    {
+        if(str.equals("")) return 0;
+        String[] words = str.split(" ");
+        return words.length;
+    }
+
+    public static void main(String args[]) {
+        WordCounter wc = new WordCounter();
+    }
+
+}
